43c5c2c84b55bf6cea430c36cad40afbb990837e
Wong Hoi Sing Edison <hswong3i@gmail.com>
2017-01-22 22:42:15 +0800

    Revamp package as DruStack\Composer\PreservePaths

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,51 +2,49 @@
 
 /**
  * @file
- * Contains derhasi\Composer\Plugin.
+ * Contains DruStack\Composer\Plugin.
  */
 
-namespace derhasi\Composer;
+namespace DruStack\Composer\PreservePaths;
 
 use Composer\Composer;
-use Composer\DependencyResolver\Operation\InstallOperation;
-use Composer\DependencyResolver\Operation\UninstallOperation;
-use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Installer\PackageEvent;
 use Composer\Script\ScriptEvents;
-use Composer\Util\Filesystem;
 
 /**
  * Class Plugin.
  */
-class Plugin implements PluginInterface, EventSubscriberInterface {
-
-  /**
-   * @var \derhasi\Composer\PluginWrapper
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    /**
+   * @var \DruStack\Composer\PreservePaths\PluginWrapper
    */
   protected $wrapper;
 
   /**
    * {@inheritdoc}
    */
-  public function activate(Composer $composer, IOInterface $io) {
-    $this->wrapper = new PluginWrapper($composer, $io);
+  public function activate(Composer $composer, IOInterface $io)
+  {
+      $this->wrapper = new PluginWrapper($composer, $io);
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
-    return array(
+  public static function getSubscribedEvents()
+  {
+      return [
       ScriptEvents::PRE_PACKAGE_INSTALL => 'prePackage',
       ScriptEvents::POST_PACKAGE_INSTALL => 'postPackage',
       ScriptEvents::PRE_PACKAGE_UPDATE => 'prePackage',
       ScriptEvents::POST_PACKAGE_UPDATE => 'postPackage',
       ScriptEvents::PRE_PACKAGE_UNINSTALL => 'prePackage',
       ScriptEvents::POST_PACKAGE_UNINSTALL => 'postPackage',
-    );
+    ];
   }
 
   /**
@@ -54,9 +52,9 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
    *
    * @param \Composer\Installer\PackageEvent $event
    */
-  public function prePackage(PackageEvent $event) {
-
-    $this->wrapper->prePackage($event);
+  public function prePackage(PackageEvent $event)
+  {
+      $this->wrapper->prePackage($event);
   }
 
   /**
@@ -64,8 +62,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
    *
    * @param \Composer\Installer\PackageEvent $event
    */
-  public function postPackage(PackageEvent $event) {
-    $this->wrapper->postPackage($event);
+  public function postPackage(PackageEvent $event)
+  {
+      $this->wrapper->postPackage($event);
   }
-
 }
---
+++ tests/TempDirectory.php
@@ -0,0 +1,212 @@
+<?php
+
+/**
+ * @file
+ * Contains \DruStack\Composer\PreservePaths\Tests\TempDirectory.
+ */
+
+namespace DruStack\Composer\PreservePaths\Tests;
+
+/**
+ * Class TempDirectory.
+ */
+class TempDirectory
+{
+    /**
+     * Root path for the temporary directory.
+     *
+     * @var string
+     */
+    protected $root;
+
+    /**
+     * Name for the temp directory.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Unique ID for building the root.
+     *
+     * @var string
+     */
+    protected $subdir;
+
+    /**
+     * Constructor.
+     *
+     * @param string $name
+     *                     An optional name for building multiple
+     */
+    public function __construct($name = '')
+    {
+        $this->name = $name;
+        $this->setSubdir();
+        $this->createDirectory();
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        $this->destroyDirectory();
+    }
+
+    /**
+     * Provides the temp directory root for this instance.
+     *
+     * @return string
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+     * Provides the absolute path for a temp directory subfolder.
+     *
+     * @param string $subfolder
+     *                          Relative path of the subfolder within the temporary directory
+     *
+     * @return string
+     *                Absolute path to the subfolder within the directory
+     */
+    public function getPath($subfolder = '')
+    {
+        // Remove leading and trailing slashes.
+        trim($subfolder, '/');
+        if (strpos($subfolder, '..') !== false) {
+            throw new \Exception('Subfolder must not contain any "..".');
+        }
+
+        if (strlen($subfolder)) {
+            return $this->root.'/'.$subfolder;
+        }
+
+        return $this->root;
+    }
+
+    /**
+     * This function recursively deletes all files and folders under the given
+     * directory, and then the directory itself.
+     *
+     * equivalent to Bash: rm -r $path
+     *
+     * @param string $path
+     */
+    public static function removeRecursive($path)
+    {
+        // If the path is not a directory we can simply unlink it.
+        if (!is_dir($path)) {
+            chmod($path, 0777);
+
+            return unlink($path);
+        }
+
+        // Otherwise we go through the whole directory.
+        $it = new \RecursiveDirectoryIterator($path);
+
+        // Fix permissions for all folders and files first, before we try to change
+        // the file mode.
+        $files = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::SELF_FIRST);
+        foreach ($files as $file) {
+            if ('.' === $file->getBasename() || '..' === $file->getBasename()) {
+                continue;
+            }
+            chmod($file->getPathname(), 0777);
+        }
+
+        // Delete files first, before folders are removed.
+        $files = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($files as $file) {
+            if ('.' === $file->getBasename() || '..' === $file->getBasename()) {
+                continue;
+            }
+            if ($file->isDir()) {
+                rmdir($file->getPathname());
+            } else {
+                unlink($file->getPathname());
+            }
+        }
+
+        return rmdir($path);
+    }
+
+    protected function getSubdir()
+    {
+        if (!isset($this->subdir)) {
+            $this->setSubdir();
+        }
+
+        return $this->subdir;
+    }
+
+    /**
+     * Creates a unique name for the subdirectory.
+     */
+    protected function setSubdir()
+    {
+        $sha = sha1($this->name);
+        // Only allow word characters.
+        $prefix = preg_replace('/[\W]/i', '', $this->name);
+        $time = time();
+        $this->subdir = "$prefix-$sha-$time";
+    }
+
+    /**
+     * Creates the directory for this instance.
+     */
+    protected function createDirectory()
+    {
+        if (!isset($this->root)) {
+            $this->root = $this->buildRoot();
+            mkdir($this->root);
+        }
+    }
+
+    /**
+     * Retrieve root path, that is not already taken.
+     *
+     * @return string
+     */
+    protected function buildRoot()
+    {
+        $root = $this->getAndPrepareSystemTempDir().'/'.$this->getSubdir();
+
+        if (file_exists($root)) {
+            $count = 1;
+            while (file_exists($root.'_'.$count)) {
+                ++$count;
+            }
+
+            return $root.'_'.$count;
+        }
+
+        return $root;
+    }
+
+    /**
+     * Helper to get and prepare system temp directory.
+     *
+     * @return string
+     */
+    protected function getAndPrepareSystemTempDir()
+    {
+        $dir = sys_get_temp_dir();
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        return $dir;
+    }
+
+    /**
+     * Removes directory for this instance.
+     */
+    protected function destroyDirectory()
+    {
+        self::removeRecursive($this->root);
+    }
+}
